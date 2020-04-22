6d24f534ef9e788f55def8c13d2491e2d36460eb
Jose C Gomez <jose@josecgomez.com>
2020-04-21 20:56:53 -0400

    Delete release.sh

--- .github/workflows/release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd SQRLDotNetClientUI
-dotnet publish -r win-x64 -c Release /p:PublishSingleFile =false /p:PublishTrimmed=true
-dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=false /p:PublishTrimmed=true
-dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=false /p:PublishTrimmed=true
