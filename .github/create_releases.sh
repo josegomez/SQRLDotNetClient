520cbfd68832735a56c9e092f829e0a51469697f
Jose C Gomez <jose@josecgomez.com>
2020-04-21 20:59:01 -0400

    Create create_releases.sh

---
+++ .github/create_releases.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd SQRLDotNetClientUI
+dotnet publish -r win-x64 -c Release /p:PublishSingleFile =false /p:PublishTrimmed=true
+dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=false /p:PublishTrimmed=true
+dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=false /p:PublishTrimmed=true
