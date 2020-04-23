704a4f38c8fbe1a5260f8d37efad19db7f3a3a04
Jose C Gomez <jose@josecgomez.com>
2020-04-23 11:05:45 -0400

    Update releases.ps1

--- .github/releases.ps1
+++ .github/releases.ps1
@@ -2,7 +2,9 @@ param (
     [string]$token = "",
     [string]$milestone = ""  
  )
-
+ echo $token
+ echo $milestone
+cd SQRLDotNetClientUI
 dotnet publish -r win-x64 -c Release /p:PublishSingleFile=false /p:PublishTrimmed=true
 dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=false /p:PublishTrimmed=true
 dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=false /p:PublishTrimmed=true
